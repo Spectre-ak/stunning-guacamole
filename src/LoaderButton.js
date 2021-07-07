a4549ca2c7c334ccc2c18d48876237245e24fbb6
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-07 13:17:52 +0530

    removed

--- src/LoaderButton.js
+++
@@ -1,9 +0,0 @@
-export default function LoaderButtom(props){
-
-    return(
-        <div class="spinner-border text-primary" role="status">
-        <span class="sr-only">Loading...</span>
-      </div>
-    )
-}
-
